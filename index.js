056f871cc5df8e3c5017cf59e6285836ded08167
Virendra Kumar <development.vk@hotmail.com>
2018-12-02 02:28:54 +0530

    Optimized Code

--- index.js
+++ index.js
@@ -24,23 +24,3 @@ const port = process.env.PORT || 3000;
 const server = app.listen(port, () => winston.info(`Listening on port ${port}...`));
 
 module.exports = server;
-
-// for Windows : $env:NODE_ENV="production" 
-// for Other: export NODE_ENV="production"
-/**
- * $env:NODE_ENV="production"
- * $env:NODE_ENV="development"
- * $env:NODE_ENV="test"
- * $env:app_password=123456
- * $env:nodeapi_jwtPrivateKey=123456
- * heroku config:set nodeapi_jwtPrivateKey=123456
- * heroku config:set NODE_ENV="production"
- * heroku config
- * heroku config:set nodeapi_db=mongodb://<dbuser>:<dbpassword>@ds123454.mlab.com:23454/node-api47
- * $env:DEBUG="app:startup,app:db"
- * $env:DEBUG="app:*"
- * npm i jest --save-dev
- * npm i supertest --save-dev
- * $env:HTTP_PROXY=http://proxy.server.com:1234
- */
-// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
--- models/x.js
+++ models/x.js
@@ -11,3 +11,26 @@
 // Calculate the rental fee (numberOfDays * movie.dailyRentalRate)
 // Increase the stock 
 // Return the rental 
+
+
+
+// for Windows : $env:NODE_ENV="production" 
+// for Other: export NODE_ENV="production"
+/**
+ * $env:NODE_ENV="production"
+ * $env:NODE_ENV="development"
+ * $env:NODE_ENV="test"
+ * $env:app_password=123456
+ * $env:nodeapi_jwtPrivateKey=123456
+ * heroku config:set nodeapi_jwtPrivateKey=123456
+ * heroku config:set NODE_ENV="production"
+ * heroku config
+ * heroku config:set nodeapi_db=mongodb://<dbuser>:<dbpassword>@ds123454.mlab.com:23454/node-api47
+ * git push heroku master
+ * $env:DEBUG="app:startup,app:db"
+ * $env:DEBUG="app:*"
+ * npm i jest --save-dev
+ * npm i supertest --save-dev
+ * $env:HTTP_PROXY=http://proxy.server.com:1234
+ */
+// console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
