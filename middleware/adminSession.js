e2103fa4a2c36b36b7c1b2b19155d793c7429c12
Virendra Kumar <development.vk@hotmail.com>
2018-12-23 20:30:11 +0530

    Optimized Code

--- middleware/adminSession.js
+++ middleware/adminSession.js
@@ -1,13 +1,15 @@
 const pjson = require('../package.json');
-
-module.exports = function (req, res, next) {
+const { navigationMenuListing } = require('../helpers/SocketHelper');
+module.exports = async function (req, res, next) {
 	try {
-		req.session.admin.version = pjson.version;
-		res.locals.adminAuthenticated = req.session.adminAuthenticated;
-		res.locals.admin = req.session.admin;
-		let current_pathname = req.path.split('/')[1];
-		res.locals.current_pathname = `/admin/${current_pathname}`;
 		if (req.session.adminAuthenticated === true && req.cookies.session) {
+			req.session.admin.version = pjson.version;
+			res.locals.adminAuthenticated = req.session.adminAuthenticated;
+			res.locals.admin = req.session.admin;
+			let current_pathname = req.path.split('/')[1];
+			res.locals.current_pathname = `/admin/${current_pathname}`;
+			if(!req.session.admin.hasOwnProperty('navigations')) { await navigationMenuListing(req); }
+			
 			next();
 		} else {
 			return res.redirect('/admin/login');
