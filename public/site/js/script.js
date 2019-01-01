158267a061046926e19d16388cd56fb5af820c20
Virendra Kumar <development.vk@hotmail.com>
2019-01-01 18:57:06 +0530

    Optimized Code

--- public/site/js/script.js
+++ public/site/js/script.js
@@ -1,76 +1,90 @@
 'use strict';
 
-$(function() {
+$(function () {
 
-
-  /*
-  |--------------------------------------------------------------------------
-  | Configure your website
-  |--------------------------------------------------------------------------
-  |
-  | We provided several configuration variables for your ease of development.
-  | Read their complete description and modify them based on your need.
-  |
-  */
- 
-  thesaas.config({
 
     /*
     |--------------------------------------------------------------------------
-    | Google API Key
+    | Configure your website
     |--------------------------------------------------------------------------
     |
-    | Here you may specify your Google API key if you need to use Google Maps
-    | in your application
-    |
-    | https://developers.google.com/maps/documentation/javascript/get-api-key
+    | We provided several configuration variables for your ease of development.
+    | Read their complete description and modify them based on your need.
     |
     */
 
-    googleApiKey: 'AIzaSyDRBLFOTTh2NFM93HpUA4ZrA99yKnCAsto',
+    thesaas.config({
+
+        /*
+        |--------------------------------------------------------------------------
+        | Google API Key
+        |--------------------------------------------------------------------------
+        |
+        | Here you may specify your Google API key if you need to use Google Maps
+        | in your application
+        |
+        | https://developers.google.com/maps/documentation/javascript/get-api-key
+        |
+        */
+
+        googleApiKey: 'AIzaSyDRBLFOTTh2NFM93HpUA4ZrA99yKnCAsto',
+
+        /*
+        |--------------------------------------------------------------------------
+        | Google Analytics Tracking
+        |--------------------------------------------------------------------------
+        |
+        | If you want to use Google Analytics, you can specify your Tracking ID in
+        | this option. Your key would be a value like: UA-12345678-9
+        |
+        */
+
+        googleAnalyticsId: '',
+
+        /*
+        |--------------------------------------------------------------------------
+        | Smooth Scroll
+        |--------------------------------------------------------------------------
+        |
+        | If true, the browser's scrollbar moves smoothly on scroll and gives your
+        | visitor a better experience for scrolling.
+        |
+        */
+
+        smoothScroll: true
+
+    });
+
+
+
+
 
     /*
     |--------------------------------------------------------------------------
-    | Google Analytics Tracking
+    | Custom Javascript code
     |--------------------------------------------------------------------------
     |
-    | If you want to use Google Analytics, you can specify your Tracking ID in
-    | this option. Your key would be a value like: UA-12345678-9
+    | Now that you configured your website, you can write additional Javascript
+    | code below this comment. You might want to add more plugins and initialize
+    | them in this file.
     |
     */
 
-    googleAnalyticsId: '',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Smooth Scroll
-    |--------------------------------------------------------------------------
-    |
-    | If true, the browser's scrollbar moves smoothly on scroll and gives your
-    | visitor a better experience for scrolling.
-    |
-    */
-   
-    smoothScroll: true
-
-  });
+    // CHAT BOOT MESSENGER////////////////////////
 
 
+    $(document).ready(function () {
+        $(".chat_on").click(function () {
+            $(".layout").toggle();
+            $(".chat_on").hide(300);
+        });
 
+        $(".chat_close_icon").click(function () {
+            $(".layout").hide();
+            $(".chat_on").show(300);
+        });
 
-
-  /*
-  |--------------------------------------------------------------------------
-  | Custom Javascript code
-  |--------------------------------------------------------------------------
-  |
-  | Now that you configured your website, you can write additional Javascript
-  | code below this comment. You might want to add more plugins and initialize
-  | them in this file.
-  |
-  */
- 
-
-
+    });
 
 });
