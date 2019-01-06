79616568d3dabba8995724d259ab05cab2df1c22
Virendra Kumar <development.vk@hotmail.com>
2019-01-06 19:24:04 +0530

    Optimized Code

--- public/site/js/script.js
+++ public/site/js/script.js
@@ -85,11 +85,7 @@ $(function () {
             $(".chat_on").show(300);
         });
 
-        $('.messenger_messenger').on('input', '[name=input_chat_box]', function (e) {
-            visibleFooterButton();
-        });
-
-        $(".messenger_messenger").on('keypress', '[name=input_chat_box]', function (e) {
+        $(".messenger_messenger").on('keypress', '[name=input_chat_box], .emoji-wysiwyg-editor', function (e) {
             if (e.which == 10 || e.which == 13) {
                 e.preventDefault();
                 $(".messenger_messenger").find('.input_button-send').click();
@@ -98,7 +94,7 @@ $(function () {
 
         $(".messenger_messenger").on('click', '.input_button-send', function (e) {
             e.preventDefault();
-            let message_value = $.trim($('[name=input_chat_box]').val());
+            let message_value = $.trim($("[name=input_chat_box]")[0].emojioneArea.getText());
             if (message_value.length > 0) {
                 let timestamp = moment(new Date()).format('hh:mm A');
                 let html = `<div class="answer left">
@@ -120,13 +116,14 @@ $(function () {
                     <div class="time">${timestamp}</div>
                 </div>`;
                 $('.messenger_content').find('.messages_list').append(html);
-                $('[name=input_chat_box]').val('');
+                $("[name=input_chat_box]")[0].emojioneArea.hidePicker();
+                $('.emojionearea-editor').empty();
                 scrollToBottom();
             }
         });
 
         function visibleFooterButton() {
-            let message_value = $.trim($('[name=input_chat_box]').val());
+            let message_value = $.trim($("[name=input_chat_box]")[0].emojioneArea.getText());
             if (message_value.length > 0) {
                 $('.input_button-attachment').css('display', "none");
                 $('.input_button-send').css('display', "block");
@@ -138,19 +135,7 @@ $(function () {
 
         function buildFooterButton() {
             return `<div class="input input-blank">
-                <textarea class="input_field" name="input_chat_box" placeholder="Send a message..." style="height: 20px;"></textarea>
-                <button class="input_button input_button-emoji">
-                    <div class="icon" style="width: 18px; height: 18px;">
-                        <svg width="56px" height="56px" viewBox="1332 47 56 56" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="width: 18px; height: 18px;">
-                            <g id="emoji" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(1332.000000, 47.000000)">
-                                <path d="M28,56 C12.536027,56 0,43.463973 0,28 C0,12.536027 12.536027,0 28,0 C43.463973,0 56,12.536027 56,28 C56,43.463973 43.463973,56 28,56 Z M28,50 C40.1502645,50 50,40.1502645 50,28 C50,15.8497355 40.1502645,6 28,6 C15.8497355,6 6,15.8497355 6,28 C6,40.1502645 15.8497355,50 28,50 Z" id="Oval-8" fill="#96AAB4" fill-rule="nonzero"></path>
-                                <path d="M28,47 C18.0588745,47 10,38.9411255 10,29 C10,27.5224898 11.5469487,26.5550499 12.8754068,27.2017612 C13.0116063,27.2662365 13.0926181,27.3037345 13.1866998,27.3464814 C13.4611235,27.4711684 13.7819537,27.6111958 14.1451774,27.7627577 C15.1908595,28.199088 16.3591406,28.6365764 17.6173846,29.0449298 C21.1841638,30.2025005 24.7379224,30.8945075 28,30.8945075 C31.2620776,30.8945075 34.8158362,30.2025005 38.3826154,29.0449298 C39.6408594,28.6365764 40.8091405,28.199088 41.8548226,27.7627577 C42.2180463,27.6111958 42.5388765,27.4711684 42.8133002,27.3464814 C42.9073819,27.3037345 42.9883937,27.2662365 43.0558366,27.2344634 C44.4530513,26.5550499 46,27.5224898 46,29 C46,38.9411255 37.9411255,47 28,47 Z M28,43 C34.6510529,43 40.2188483,38.3620234 41.6456177,32.1438387 C40.9980758,32.3847069 40.320642,32.6213409 39.6173846,32.8495777 C35.6841638,34.1260741 31.7379224,34.8945075 28,34.8945075 C24.2620776,34.8945075 20.3158362,34.1260741 16.3826154,32.8495777 C15.679358,32.6213409 15.0019242,32.3847069 14.3543823,32.1438387 C15.7811517,38.3620234 21.3489471,43 28,43 Z" id="Oval-8" fill="#96AAB4" fill-rule="nonzero"></path>
-                                <path d="M19,15 L19,20 C19,21.1045695 19.8954305,22 21,22 C22.1045695,22 23,21.1045695 23,20 L23,15 C23,13.8954305 22.1045695,13 21,13 C19.8954305,13 19,13.8954305 19,15 Z" id="Line" fill="#96AAB4" fill-rule="nonzero"></path>
-                                <path d="M32,15 L32,20 C32,21.1045695 32.8954305,22 34,22 C35.1045695,22 36,21.1045695 36,20 L36,15 C36,13.8954305 35.1045695,13 34,13 C32.8954305,13 32,13.8954305 32,15 Z" id="Line-Copy-2" fill="#96AAB4" fill-rule="nonzero"></path>
-                            </g>
-                        </svg>
-                    </div>
-                </button>
+                <textarea class="input_field" name="input_chat_box"></textarea>
                 <button class="input_button input_button-attachment">
                     <div class="icon" style="width: 18px; height: 18px;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" ratio="1">
@@ -188,8 +173,130 @@ $(function () {
             e.preventDefault();
             $('.messenger_content').find('.messages_list').html('');
             $('.messenger_content').find('.chat_footer').html(buildFooterButton());
-
+            drawEmoji();
         });
+
+        function drawEmoji() {
+            $("[name=input_chat_box]").emojioneArea({
+                pickerPosition: "top",
+                filtersPosition: "top",
+                tonesStyle: "checkbox",
+                searchPosition: "top",
+                spellcheck: true,
+                autocomplete: "on",
+                placeholder: "Type something here",
+                searchPlaceholder: "Search",
+                buttonTitle: "Use the TAB key to insert emoji faster",
+                dir: "ltr",
+                events: {
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    focus: function (editor, event) {
+                        // console.log('event:focus');
+                    },
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    blur: function (editor, event) {
+                        // console.log('event:blur');
+                    },
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    mousedown: function (editor, event) {
+                        // console.log('event:mousedown');
+                    },
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    mouseup: function (editor, event) {
+                        // console.log('event:mouseup');
+                    },
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    click: function (editor, event) {
+                        // console.log('event:click');
+                    },
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    keyup: function (editor, event) {
+                        visibleFooterButton();
+                        // console.log('event:keyup');
+                    },
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    keydown: function (editor, event) {
+                        visibleFooterButton();
+                        // console.log('event:keydown');
+                    },
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    keypress: function (editor, event) {
+                        // console.log('event:keypress');
+                        visibleFooterButton();
+                        if (event.which == 10 || event.which == 13) {
+                            event.preventDefault();
+                            $(".messenger_messenger").find('.input_button-send').click();
+                        }
+                    },
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    paste: function (editor, event) {
+                        // console.log('event:paste');
+                    },
+                    /**
+                     * @param {jQuery} editor EmojioneArea input
+                     * @param {Event} event jQuery Event object
+                     */
+                    change: function (editor, event) {
+                        // console.log('event:change');
+                    },
+                    /**
+                     * @param {jQuery} filter EmojioneArea filter
+                     * @param {Event} event jQuery Event object
+                     */
+                    filter_click: function (filter, event) {
+                        // console.log('event:filter.click, filter=' + filter.data("filter"));
+                    },
+                    /**
+                     * @param {jQuery} button EmojioneArea emoji button
+                     * @param {Event} event jQuery Event object
+                     */
+                    emojibtn_click: function (button, event) {
+                        // console.log('event:emojibtn.click, emoji=' + button.children().data("name"));
+                    },
+                    /**
+                     * @param {jQuery} button EmojioneArea left arrow button
+                     * @param {Event} event jQuery Event object
+                     */
+                    arrowLeft_click: function (button, event) {
+                        // console.log('event:arrowLeft.click');
+                    },
+                    /**
+                     * @param {jQuery} button EmojioneArea right arrow button
+                     * @param {Event} event jQuery Event object
+                     */
+                    arrowRight_click: function (button, event) {
+                        // console.log('event:arrowRight.click');
+                    }
+                }
+            });
+        }
     });
 
 });
