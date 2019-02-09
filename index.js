f83ba9c2132fcfdaf0382bd22fbcb413cecee279
Virendra Kumar <development.vk@hotmail.com>
2019-02-10 00:16:13 +0530

    Optimized Code

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const express = require('express'),
 
 
 const numUsers = 0;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6000;
 
 const { SocketUsers } = require('./helpers/SocketUsers');
 const { SocketAdmins } = require('./helpers/SocketAdmins');
